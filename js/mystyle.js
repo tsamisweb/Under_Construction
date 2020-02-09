cb1437387a6fab1aa504622fcd6cbc7397bb97ce
tsamisweb <tsamisgis@gmail.com>
2020-02-09 18:11:45 +0200

    js files

---
+++ js/mystyle.js
@@ -0,0 +1,6 @@
+(function() {
+    $('.intro-site span:eq(0)').first().delay(100).animate({'opacity':'1'},600);
+    $('.intro-site span:eq(1)').first().delay(200).animate({'opacity':'1'},600);
+    $('.intro-site span:eq(2)').first().delay(300).animate({'opacity':'1'},600);
+    $('.intro-site span:eq(3)').last().delay(400).animate({'opacity':'1'},600);
+}());
