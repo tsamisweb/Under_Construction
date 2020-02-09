e5fe6211dcb4b9f2a2dac43c634b0fed0e235923
tsamisweb <tsamisgis@gmail.com>
2020-02-09 18:11:18 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<meta name="Author" content="WebThemez"> 
+		<title>Go Creative Web development Company</title>
+		<meta name="description" content="Examples for creative website header animations using Canvas and JavaScript" />
+		<meta name="keywords" content="header, canvas, animated, creative, inspiration, javascript" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" /> 
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">		<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->		
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body> 
+
+<body id="home">
+<div id="large-header" class="large-header">
+	<canvas id="demo-canvas"></canvas> 
+</div>
+<div id="Content" class="wrapper"> 
+<div class="top-wrapper">
+<div id="header">
+</div>
+</div>
+<div class="container intro-site">
+	<div class="row">
+		<div class="col-12 col-sm-12 col-md-12"><h1><a href="#" class="logo">GoCreative</a></h1></div>
+	</div>
+	<div class="row">
+		<div class="col-12 col-sm-12 col-md-12"><span>Web Design // </span> <span>Web development // </span> <span>Social Media // </span> <span>Photography</span></div>
+	</div>
+</div>
+
+<div class="container content">
+	<h2 class="intro">Our Team have been working on something Amazing <br/>We will be back soon</h2>
+		<div id="subscribe">  
+		<form method="POST" action="subscribe.php"> 
+			<p class="subscribe-input"><input type="text" name="Email" size="20" value="" placeholder="Subscribe your e-mail Id"><input type="submit" value="Submit" name="Submit"></p> 
+		</form>
+
+		<div id="socialIcons">
+			<ul> 
+				<li><a href="" title="Twitter" class="twitterIcon"></a></li>
+				<li><a href="" title="facebook" class="facebookIcon"></a></li>
+				<li><a href="" title="linkedIn" class="linkedInIcon"></a></li>
+				<li><a href="" title="Pintrest" class="pintrestIcon"></a></li>
+			</ul>
+		</div>
+</div> 
+	<span class="tempBy">&copy; 2020 All right reserved. <br/><a href="#" target="_blank">Crafted with love </a>{ <i class="fa fa-heart"> </i> } by gocreative.gr</span>
+</div>
+</div> 
+ 
+<script src="js/TweenLite.min.js"></script>
+<script src="js/EasePack.min.js"></script>
+<script src="js/rAF.js"></script>
+<script src="js/demo-1.js"></script> 
+
+		
+<!--Scripts-->
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/Backstretch.js"></script>
+<script type="text/javascript" src="js/jquery.countdown.js"></script>
+<script type="text/javascript" src="js/global.js"></script>
+<script type="text/javascript" src="js/mystyle.js"></script>
+
+	</body>
+</html>
