9b45e49a021159d313140d5d569eba5fc5dda98d
tsamisweb <tsamisgis@gmail.com>
2020-02-09 18:12:13 +0200

    subscribe.php

---
+++ subscribe.php
@@ -0,0 +1,32 @@
+<?php
+
+## CONFIG ##
+$body = '';
+
+# LIST EMAIL ADDRESS
+$recipient = "gocreative.gr@gmail.com";
+
+# SUBJECT (Subscribe/Remove)
+$subject = "Subscribe";
+
+# RESULT PAGE
+$location = "index.html";
+
+## FORM VALUES ##
+
+# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
+# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
+$sender = $recipient;
+
+# MAIL BODY 
+$body .= "Email: ".$_REQUEST['Email']." \n";
+# add more fields here if required
+
+## SEND MESSGAE ##
+
+mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+
+## SHOW RESULT PAGE ##
+
+header( "Location: $location" );
+?>
